eff5bf6e89475de0c6d672cd4451c372997716d5
= <=>
2023-06-10 13:49:48 -0600

    integrated socket.io

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const app = express();
 //Socket.io has to use the http server
 const server = require('http').Server(app);
 
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
