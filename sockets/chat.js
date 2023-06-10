7e308b1bc485ef73b53a4ad81e2ea5424560768a
= <=>
2023-06-10 14:00:10 -0600

    users can join the chat

---
+++ sockets/chat.js
@@ -0,0 +1,7 @@
+//chat.js
+module.exports = (io, socket) => {
+    // Listen for "new user" socket emits
+    socket.on('new user', (username) => {
+      console.log(`${username} has joined the chat! ✋`);
+    })
+}
